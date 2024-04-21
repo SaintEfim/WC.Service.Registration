d49df5c2211fbbfbce45931785ca37a2391d842d
SaintEfim <93579076+SaintEfim@users.noreply.github.com>
2024-04-21 13:21:08 +0300

    Revert "finished work"

--- src/WC.Service.Registration.Domain.Abstractions/Models/Requests/RegistrationRequestModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WC.Service.Registration.Domain.Models.Requests;
-
-public class RegistrationRequestModel
-{
-    public string Email { get; init; } = string.Empty;
-    public string Password { get; init; } = string.Empty;
-    public string? Role { get; init; }
-}
