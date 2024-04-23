4ecb877c654ea9801c8011cda8d6e1bc377c629a
SaintEfim <saintefim2@gmail.com>
2024-04-23 16:51:11 +0300

    bug fixed

--- src/WC.Service.Registration.Data.PostgreSql/Context/UserRegistrationDbContext.cs
+++ src/WC.Service.Registration.Data.PostgreSql/Context/UserRegistrationDbContext.cs
@@ -10,5 +10,5 @@ public sealed class UserRegistrationDbContext : DbContext
         // Database.Migrate();
     }
 
-    public DbSet<UserRegistrationEntity> UsersCredentials { get; init; } = null!;
+    public DbSet<UserRegistrationEntity> UsersRegistrations { get; init; } = null!;
 }
