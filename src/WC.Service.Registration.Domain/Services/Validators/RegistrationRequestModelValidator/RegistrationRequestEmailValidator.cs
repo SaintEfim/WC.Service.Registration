3f2365c4a29d0e35cbe400f474bf23241e8ac29f
SaintEfim <saintefim2@gmail.com>
2024-04-25 18:05:25 +0300

    fixed

--- src/WC.Service.Registration.Domain/Services/Validators/RegistrationRequestModelValidator/RegistrationRequestEmailValidator.cs
+++ src/WC.Service.Registration.Domain/Services/Validators/RegistrationRequestModelValidator/RegistrationRequestEmailValidator.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using WC.Library.Domain.Services.Validators;
 using WC.Service.Registration.Domain.Models.Requests;
 
 namespace WC.Service.Registration.Domain.Services.Validators.RegistrationRequestModelValidator;
@@ -8,28 +9,6 @@ public class RegistrationRequestEmailValidator : AbstractValidator<RegistrationR
     public RegistrationRequestEmailValidator()
     {
         RuleFor(x => x.Email)
-            .EmailAddress()
-            .Custom((email, context) =>
-            {
-                if (!RegistrationRequestEmailValidatorHelper.TryGetDomain(email, out var domain))
-                {
-                    context.AddFailure("Invalid email format or domain");
-                    return;
-                }
-
-                var allowedDomains = new List<string?>
-                {
-                    "gmail.com",
-                    "mail.ru",
-                    "yahoo.com",
-                    "hotmail.com",
-                    "outlook.com"
-                };
-
-                if (domain != null && !allowedDomains.Contains(domain))
-                {
-                    context.AddFailure($"Invalid domain '{domain}'");
-                }
-            });
+            .SetValidator(new EmailValidator());
     }
 }
--- src/WC.Service.Registration.Domain/Services/Validators/RegistrationRequestModelValidator/RegistrationRequestPasswordValidator.cs
+++ src/WC.Service.Registration.Domain/Services/Validators/RegistrationRequestModelValidator/RegistrationRequestPasswordValidator.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using WC.Library.Domain.Services.Validators;
 using WC.Service.Registration.Domain.Models.Requests;
 
 namespace WC.Service.Registration.Domain.Services.Validators.RegistrationRequestModelValidator;
@@ -8,11 +9,6 @@ public class RegistrationRequestPasswordValidator : AbstractValidator<Registrati
     public RegistrationRequestPasswordValidator()
     {
         RuleFor(x => x.Password)
-            .NotEmpty()
-            .Length(8, 64).WithMessage("Password must be between 8 and 64 characters")
-            .Matches(@"(?=.*\d)").WithMessage("At least one digit (0-9)")
-            .Matches("(?=.*[a-z])").WithMessage("At least one lowercase letter (a-z)")
-            .Matches("(?=.*[A-Z])").WithMessage("At least one uppercase letter (A-Z)")
-            .Matches(@"(?!.*\s)").WithMessage("No whitespace characters (spaces, tabs, etc.)");
+            .SetValidator(new PasswordValidator());
     }
 }
