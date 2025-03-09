5b1bb3fb510c802a6506ff806bdd5512d9ec6b00
SaintEfim <saintefim2@gmai.com>
2025-03-09 22:46:46 +0300

    fix

--- src/WC.Service.Registration.CreateAdmin/Program.cs
+++ src/WC.Service.Registration.CreateAdmin/Program.cs
@@ -26,6 +26,7 @@ internal static class Program
         var configuration = new ConfigurationBuilder().SetBasePath(projectPath)
             .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
             .AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables()
             .Build();
 
         builder.RegisterInstance(configuration)
@@ -37,9 +38,26 @@ internal static class Program
         builder.Register(context =>
             {
                 var configuration = context.Resolve<IConfiguration>();
-                var options = new AdminSettingsOptions();
-                configuration.GetSection("AdminSettings").Bind(options);
-                return options;
+                var adminSection = configuration.GetSection("AdminSettings");
+
+                var adminEmailLocalPart = adminSection.GetValue<string>("AdminEmailLocalPart");
+                var adminEmailDomain = adminSection.GetValue<string>("AdminEmailDomain");
+                var adminRegistrationName = adminSection.GetValue<string>("AdminRegistrationName");
+                var adminRegistrationSurname = adminSection.GetValue<string>("AdminRegistrationSurname");
+                var adminRegistrationPatronymic = adminSection.GetValue<string>("AdminRegistrationPatronymic");
+                var adminRegistrationPassword = adminSection.GetValue<string>("AdminRegistrationPassword");
+                var adminPositionId = adminSection.GetValue<string>("AdminPositionId");
+
+                return new AdminSettingsOptions
+                {
+                    AdminEmailLocalPart = adminEmailLocalPart,
+                    AdminEmailDomain = adminEmailDomain,
+                    AdminRegistrationName = adminRegistrationName,
+                    AdminRegistrationSurname = adminRegistrationSurname,
+                    AdminRegistrationPatronymic = adminRegistrationPatronymic,
+                    AdminRegistrationPassword = adminRegistrationPassword,
+                    AdminPositionId = Guid.Parse(adminPositionId!)
+                };
             })
             .As<AdminSettingsOptions>()
             .SingleInstance();
